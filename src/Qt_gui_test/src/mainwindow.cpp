b7849ca4a6517da222d43214d6d7fbdd5e5d3391
Wang Haili <bitwanghaili@163.com>
2020-02-28 09:44:50 +0800

    init

---
+++ src/Qt_gui_test/src/mainwindow.cpp
@@ -0,0 +1,31 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(int argc, char** argv, QWidget *parent):
+    QMainWindow(parent),
+    ui(new Ui::MainWindow),
+    qnode(argc,argv)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_pushButtonConnect_clicked()
+{
+    qnode.init();
+}
+
+void MainWindow::on_pushButtonSend_clicked()
+{
+    qnode.ros_test("I changed this world!");
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    qnode.init();
+    qnode.ros_test("I changed this world again!");
+}
